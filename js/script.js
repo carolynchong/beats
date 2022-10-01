a6e6c57ba9762688f1f5bf5c0c8d7fcabe24540c
Carolyn Chong <carolynchong15@gmail.com>
2022-10-01 18:29:14 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+	dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-118371833-1');
+
+// Fade in on page load
+$(function(){  // $(document).ready shorthand
+  $('.content').hide().fadeIn('slow');
+});
+
+// Eliminate funky background jump on mobile upon scroll due to URL bar. More info here: https://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
+// var bg = $(".content");
+
+// function resizeBackground() {
+//     bg.height( $(window).height() + 60);
+// }
+
+// $(window).resize(resizeBackground);
+// resizeBackground();
